9c301d57ec93584e2d03152ef272e0d3fbb53c98
kenan551 <kenan.ramazanov1104@gmail.com>
2024-12-05 00:58:38 +0400

    Initial commit

---
+++ TicTacToe GameTests/TicTacToe_GameTests.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToe_GameTests.swift
+//  TicTacToe GameTests
+//
+//  Created by Kanan  on 04.12.24.
+//
+
+import Testing
+@testable import TicTacToe_Game
+
+struct TicTacToe_GameTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
